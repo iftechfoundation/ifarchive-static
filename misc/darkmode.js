cf4e49ec7168f6868778c0521ac9472aa3bada12
Andrew Plotkin <zarf@ZarfLeaf.local>
2024-08-18 23:11:10 -0400

    Set class based on cookie.

--- misc/darkmode.js
+++ misc/darkmode.js
@@ -1,5 +1,29 @@
 'use strict';
 
+function check_theme_cookie()
+{
+    var darkmode = false;
+    for (var val of document.cookie.split(';')) {
+        if (val.trim() == 'theme=dark') {
+            darkmode = true;
+        }
+    }
+
+    set_body_class(darkmode);
+}
+
+function set_body_class(darkmode)
+{
+    if (!darkmode) {
+        document.body.classList.add('LightMode');
+        document.body.classList.remove('DarkMode');
+    }
+    else {
+        document.body.classList.add('DarkMode');
+        document.body.classList.remove('LightMode');
+    }
+}
+
 window.addEventListener('load', function(ev) {
-    document.body.classList.add('DarkMode');
+    check_theme_cookie();
 });
