3ce7286a281d6b7823caff6f486f175fa363bdf8
Andrew Plotkin <zarf@ZarfLent.local>
2024-08-21 10:09:34 -0400

    Comments.

--- misc/darkmode.js
+++ misc/darkmode.js
@@ -1,5 +1,32 @@
 'use strict';
 
+/* Simple script to support light and dark mode on IF Archive web pages.
+
+   Our color-theme strategy is a bit complicated. We want to default to
+   the user's system color theme. This doesn't require JS at all; it relies
+   on CSS and the "prefers-color-scheme" @media query.
+
+   However, we want to support very old browsers that don't recognize
+   @media queries. And we *also* want to give the user the option to
+   manually select a dark/light theme preference. As the IF Archive does
+   not have user accounts, that preference must be stored in a cookie.
+   That's where this script comes in.
+
+   At load time, we check for a "theme=dark" or "theme=light" cookie. If
+   found, we set the <body> class to "DarkMode" or "LightMode", which
+   has fixed colors. If no cookie is present, we set the <body> class
+   to "SysMode", which uses the @media query to select colors.
+
+   (This setup means that color declarations are written very redundantly
+   in ifarchive.css. Sorry about that.)
+
+   A page-footer button lets the user set or toggle the cookie. There's
+   no way to unset the cookie, but that's okay -- if someone sets a color,
+   that's probably the color they want. Note that the page-footer button
+   might not be present on every page; the color system still works
+   site-wide as long as this script is loaded.
+*/
+
 const testmode = true; //###
 
 function get_theme_cookie()
